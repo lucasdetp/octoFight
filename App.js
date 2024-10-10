ef6c46906294efc76d7ca28f3c9e3635a476cf5c
Luluww 👽 <detplucas@gmail.com>
2024-10-10 14:43:47 +0200

    correction bug

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, ScrollView, View } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, View, Text } from 'react-native';
 import { Card, Profil } from './components/molecules';
-import { View, Text, StyleSheet } from 'react-native';
 import Menu from './components/molecules/Menu';
 import FooterNavBar from './components/molecules/Navbar/FooterNavbar';
 
@@ -22,7 +21,7 @@ export default function App() {
     rarity: 'Commun',
   };
   
-  const App = () => {
+
   const [menu, setMenu] = useState('home');
 
   const displayContent = () => {
@@ -39,6 +38,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="auto" /> 
+      <ScrollView>
+        <Card rapper={rapper} />
+        <Card rapper={rapper2} />
+      </ScrollView> 
+      <Profil.ProfileHeader user={{ name: "jul", email: "test@test.fr", photo: "https://i.scdn.co/image/ab6761610000e5ebe66ef18636bf25588abdd2ae" }} />
+     </SafeAreaView>
       <Menu
         onMenuChange={(slug) => setMenu(slug)}
         configs={[
@@ -50,16 +57,6 @@ export default function App() {
       <View>{displayContent()}</View>
       <FooterNavBar />
     </View>
-
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar style="auto" /> 
-      <ScrollView>
-        <Card rapper={rapper} />
-        <Card rapper={rapper2} />
-      </ScrollView> 
-      <Profil.ProfileHeader user={{ name: "jul", email: "test@test.fr", photo: "https://i.scdn.co/image/ab6761610000e5ebe66ef18636bf25588abdd2ae" }} />
-     </SafeAreaView>
   );
 };
 
@@ -76,7 +73,5 @@ const styles = StyleSheet.create({
   cardContainer: {
     width: '90%',
     alignItems: 'center',
-  },
+  }
 });
-
-export default App;
--- components/molecules/index.js
+++ components/molecules/index.js
@@ -1,6 +1,6 @@
 import {Card} from "./Card";
 import * as Profil from "./Profil";
 import Menu from './Menu';
-import * as FooterNavBar from './FooterNavbar';
+import * as FooterNavBar from './Navbar';
 
 export { Card, Profil, Menu, FooterNavBar};
